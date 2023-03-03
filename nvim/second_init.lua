b03eb48c922e5db7f453210608c1703709880d3f
rickyg365 <rickygonzal3z@hotmail.com>
2023-03-03 01:55:26 -0800

    updated modeline in second_init.lua

--- nvim/second_init.lua
+++ nvim/second_init.lua
@@ -510,6 +510,6 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
-
+-- Might need to edit the modeline...
 -- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+-- vim: ts=4 sts=4 sw=4 et conceallevel=2 whichwrap+=<,>,h,l,[,]
