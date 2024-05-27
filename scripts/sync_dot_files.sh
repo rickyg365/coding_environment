bdb4ed540737bbbbefafb0b8997435912c99bb67
rickyg365 <rickygonzal3z@hotmail.com>
2024-05-27 13:57:38 -0700

    update

--- scripts/sync_dot_files.sh
+++ scripts/sync_dot_files.sh
@@ -3,9 +3,9 @@
 HOME="~/.bashrc"
 DOT="~/github/coding_environment/.bahsrc"
 SCRIPTS="~/github/coding_environment/scripts/"
+TDOT="~/github/coding_environment/.bahsrc_old"
 
-
-cp -r $DOT $HOME
+cp -r $TDOT $HOME
 
 echo "dotfile sync complete!"
 
